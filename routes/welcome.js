8965ae8a3a972a081f6139f858b8c83f977364c8
Redxnel <redxnel@yandex.ru>
2019-08-08 13:01:12 +0300

    Delete styles

--- routes/welcome.js
+++ routes/welcome.js
@@ -5,9 +5,9 @@ export default (router) => {
     if (ctx.state.isSignedIn()) {
       const { userId } = ctx.session;
       const user = await User.findByPk(userId);
-      ctx.render('welcome/index', { user, userId });
+      ctx.render('welcome', { user, userId });
     } else {
-      ctx.render('welcome/index');
+      ctx.render('welcome');
     }
   });
 };
